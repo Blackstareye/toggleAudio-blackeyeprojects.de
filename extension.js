ff238256d10a984225cf2671e8d814f5c176391c
Blackeye <>
2024-06-06 14:53:03 +0200

    cleanup part 1

--- extension.js
+++ extension.js
@@ -19,7 +19,6 @@
 
 // imports
 import GObject from 'gi://GObject';
-import Gvc from 'gi://Gvc';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import {QuickSettingsItem, QuickToggle, SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
