460887f9d3776e467053d9b17531cf2266472397
Blackeye <>
2024-06-06 15:45:46 +0200

    cleanup and added destroy

--- extension.js
+++ extension.js
@@ -25,10 +25,13 @@ import {QuickSettingsItem, QuickToggle, SystemIndicator} from 'resource:///org/g
 import MixerControlFacade from './lib/MixerControl.js';
 
 
+const DEBUG=true
 
-
-const ExampleToggle = GObject.registerClass(
-class ExampleToggle extends QuickToggle {
+/**
+ * GObject Class for audio switch via toggle
+ */
+const AudioOutputToggle = GObject.registerClass(
+class AudioOutputToggle extends QuickToggle {
     constructor() {
         super({
             title: _('Switch Headphone'),
@@ -38,52 +41,76 @@ class ExampleToggle extends QuickToggle {
     }
 });
 
-const ExampleButton = GObject.registerClass(
-    class ExampleButton extends QuickSettingsItem {
-        _init(extensionMetadata) {
+/**
+ * GObject Class for Debug Infos via Button Press
+ */
+const DebugButton = GObject.registerClass(
+    class DebugButton extends QuickSettingsItem {
+
+        _clickHandler = null; 
+
+        _init(mixerControl) {
             super._init({
                 style_class: 'icon-button',
                 can_focus: true,
                 icon_name: 'dialog-information',
-                accessible_name: _('Example Action'),
+                accessible_name: _('show debug infos'),
             });
+
             
-            console.log(extensionMetadata);
-            //init MixerController
-            const mixerControl = new MixerControlFacade(extensionMetadata.name);
 
             // connect to debug button
-            this.connect('clicked', () => mixerControl.printInfos());
+            this._clickHandler = this.connect('clicked', () => mixerControl.printInfos());
+        }
+
+        destroy() {
+            this.disconnect(this._clickHandler);
+            this._clickHandler = null;
         }
     });
+        
 
-const ExampleIndicator = GObject.registerClass(
-class ExampleIndicator extends SystemIndicator {
-    constructor(extensionMetadata) {
+
+/**
+ * GObject Class for Indicator
+ * 
+ * this is the place where the ui widgets will be added
+ */
+const AudioOutputToggleIndicator = GObject.registerClass(
+class AudioOutputToggleIndicator extends SystemIndicator {
+    constructor(mixerControlFacade) {
         super();
 
         this._indicator = this._addIndicator();
         this._indicator.iconName = 'audio-headphones';
 
-        const toggle = new ExampleToggle();
+        const toggle = new AudioOutputToggle();
         toggle.bind_property('checked',
             this._indicator, 'visible',
             GObject.BindingFlags.SYNC_CREATE);
         this.quickSettingsItems.push(toggle);
 
-
-        this.quickSettingsItems.push(new ExampleButton(extensionMetadata));
+        // for debug purpose, there is an print output button 
+        if (DEBUG) {
+            this.quickSettingsItems.push(new DebugButton(mixerControlFacade));
+        }
     }
+
 });
 
 export default class QuickSettingsExampleExtension extends Extension {
+
+    _mixerControlFacade = null;
     enable() {
-        this._indicator = new ExampleIndicator(this.metadata);
+        this._mixerControlFacade = new MixerControlFacade(this.metadata.name);
+        this._indicator = new AudioOutputToggleIndicator(this._mixerControlFacade);
         Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
     }
 
     disable() {
+        this._mixerControlFacade.destroy();
         this._indicator.quickSettingsItems.forEach(item => item.destroy());
         this._indicator.destroy();
+
     }
 }
