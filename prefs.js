f2e5ac6b18c2a15c79de778892c8b8fd310a659a
Blackeye <>
2024-06-11 16:43:44 +0200

    functional Prefinterface; todo toggleSettings

--- prefs.js
+++ prefs.js
@@ -7,6 +7,7 @@ import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Ex
 import SettingProvider from './lib/SettingProvider.js';
 import OutputDeviceList from './lib/OutputDeviceList.js';
 import Printer from './lib/Printer.js';
+import DebugHelper from './lib/DebugHelper.js';
 
 
 export default class ToggleAudioPreferences extends ExtensionPreferences {
@@ -59,70 +60,64 @@ export default class ToggleAudioPreferences extends ExtensionPreferences {
                     // DELETE
                     this._reverseLookUpDeviceMap.delete(element[0]);
                 }
-                this.buildModel();
+                this._buildListModel();
             }
         );
 
         // fill List
-        console.log(`Keys are: ${this._outputDeviceMap.keys().join()}`);
-        for (let key in this._outputDeviceMap.keys()) {
+        console.debug(`Keys are: ${this._outputDeviceMap.keys().join()}`);
+        this._outputDeviceMap.keys().map( key => {
+            console.debug(`Key is: ${key}`);
             // // es-lint no-prototype-builtins fix
-            if (Object.prototype.hasOwnProperty.call(this._outputDeviceMap, key)) {
-                this._reverseLookUpDeviceMap.set(this._outputDeviceMap[key][0], key);
-            }
-        }
+            // if (Object.prototype.hasOwnProperty.call(this._outputDeviceMap, key)) {
+            console.debug(this._outputDeviceMap.printData());
+            let item = this._outputDeviceMap.getItem(key);
+            console.debug(`Item is: ${item}`);
+            if (item) {
+                this._reverseLookUpDeviceMap.set(item[0], key);
+            } else {
+                console.debug(`Item is empty.. ${item}`);
+            }   
+            // }
+
+        });
     }
 
     _buildListModel() {
         // build the model again
         this._listModelOfDevices = null;
         this._listModelOfDevices = new Gtk.StringList();
-        this._indexHeadphoneModel = 0;//deviceList.keys().findIndex(this._currentSpeakerValue);
-        this._indexSpeakerModel = 0;//deviceList.keys().findIndex(this._currentHeadphoneValue);
-        console.log(`Creating Device List now \nDebugging Values: Headphone:${this._currentHeadphoneValue}, Speaker: ${this._currentSpeakerValue}`);
+        this._indexHeadphoneModel = 0;
+        this._indexSpeakerModel = 0;
+        console.debug(`Creating Device List now \nDebugging Values: Headphone:${this._currentHeadphoneValue}, Speaker: ${this._currentSpeakerValue}`);
+
         // we need a array and need to put all in the device list
         [ ...this._reverseLookUpDeviceMap.keys()].map( (e, index) => {
-                console.log(`Stepping to: ${e}, ${index}`);
+                console.debug(`Stepping to: ${e}, ${index}`);
                 if (this._currentHeadphoneValue && this._currentHeadphoneValue === e) {
-                    console.log(`Found Entry in headphone list: ${e}, ${index}`);
+                    console.debug(`Found Entry in headphone list: ${e}, ${index}`);
                     this._indexHeadphoneModel = index;
                 }
                 if (this._currentSpeakerValue && this._currentSpeakerValue === e) {
-                    console.log(`Found Entry in speaker list: ${e}, ${index}`);
+                    console.debug(`Found Entry in speaker list: ${e}, ${index}`);
                     this._indexSpeakerModel = index;
                 }
                 this._listModelOfDevices.append(e);
         }
         );
         console.log(`headphone: ${this._indexHeadphoneModel}, speaker: ${this._indexSpeakerModel}`);
-        // console.log(`Updating Models now...`);
-        // if (this._headphoneModel != null) {
-        //     this._updateModel(this._headphoneModel, this._indexHeadphoneModel);
-        // }
-        // if (this._speakerModel != null) {
-        //     this._updateModel(this._speakerModel, this._indexSpeakerModel);
-        // }
-        
     }
     
-    // _updateModel(modelToUpdate, selectedIndex) {
-    //         // modelToUpdate.model = this._listModelOfDevices;
-    //         // modelToUpdate.selected = selectedIndex;
-    //         // console.log(`Model is null, not updating it..`);
-    //     // headphone.bind('model', this, '_listModelOfDevices', Gio.SettingsBindFlags.DEFAULT);
-    //     // headphone.bind('selected', this, '_indexHeadphoneModel', Gio.SettingsBindFlags.DEFAULT);
-    //     // speaker.bind('model', this, '_listModelOfDevices', Gio.SettingsBindFlags.DEFAULT);
-    //     // speaker.bind('selected', this, '_indexSpeakerModel', Gio.SettingsBindFlags.DEFAULT);
-    // }
+    _updateModel(modelToUpdate, selectedIndex) {
+        if (modelToUpdate) {
+            modelToUpdate.set_model(this._listModelOfDevices);
+            modelToUpdate.set_selected(selectedIndex);
+        }
+    }
+
 
     create_ui(window) {
         this.unpackSettings(window);
-        // for (let key in this._outputDeviceMap) {
-        //     // // es-lint no-prototype-builtins fix
-        //     if (Object.prototype.hasOwnProperty.call(this._outputDeviceMap, key)) {
-        //         this._reverseLookUpDeviceMap.set(this._outputDeviceMap[key][0], key);
-        //     }
-        // }
         this.buildIndex();
         this._buildListModel();
 
@@ -141,18 +136,14 @@ export default class ToggleAudioPreferences extends ExtensionPreferences {
         page.add(group);
 
 
-        // CHECK IF DATA IS THERE
-        console.log(this._outputDeviceMap.printData());
-        
-        //HIER DEBUGGEN
-        console.log(Printer.printBeautyMap(this._reverseLookUpDeviceMap));
-
-        
-        // this._listModelOfDevices.map(e => {
-        //     console.log(`DEBUGGING ${e.get_string()}`);
-        // })
-
-
+        if (DEBUG) {
+            let debugWindow = DebugHelper.createDebugBanner(group);
+            let debugWindow2 = DebugHelper.createDebugBanner(group);
+            
+    
+            debugWindow.set_title(`Reverse Map:${Printer.printBeautyMap(this._reverseLookUpDeviceMap)}`);
+            debugWindow2.set_title(`Device List:${this._outputDeviceMap.printData()}`);
+        }
 
         this._speakerModel = new Adw.ComboRow({
             title: _('Speaker'),
@@ -169,36 +160,6 @@ export default class ToggleAudioPreferences extends ExtensionPreferences {
 
         group.add(this._speakerModel);
         group.add(this._headphoneModel);
-
-        console.log(`MODELS: ${this._speakerModel} ${this._headphoneModel}`);
-
-        // this._updateModel(this._headphoneModel, this._indexHeadphoneModel);
-        // this._updateModel(this._speakerModel, this._indexSpeakerModel);
-
-        let storeSelectedDevice = function (key, origin, this_ref) {
-            let item = Object.assign(origin.selected_item, Gtk.StringObject);
-            let text = item.get_string();
-            console.debug(`selected: ${text}`);
-            console.debug(`is in device list: ${this_ref._reverseLookUpDeviceMap.has(text)}`);
-            let id = this_ref._reverseLookUpDeviceMap.get(text);
-            let metaData = this_ref._outputDeviceMap[id][1];
-            window._settingsInstance.set_value(key, new GLib.Variant("(iss)", [id,text, metaData]));
-        };
-
-        // headphone.bind('model', this, '_listModelOfDevices', Gio.SettingsBindFlags.DEFAULT);
-        // headphone.bind('selected', this, '_indexHeadphoneModel', Gio.SettingsBindFlags.DEFAULT);
-        // speaker.bind('model', this, '_listModelOfDevices', Gio.SettingsBindFlags.DEFAULT);
-        // speaker.bind('selected', this, '_indexSpeakerModel', Gio.SettingsBindFlags.DEFAULT);
-
-        this._headphoneModel.connect(`notify::selected-item`, () => {
-            storeSelectedDevice('headphone', this._headphoneModel, this);
-        });
-        this._speakerModel.connect(`notify::selected-item`, () => {
-            storeSelectedDevice('speaker', this._speakerModel, this);
-        });
-
-        console.log(this._listModelOfDevices);
-
     }
 
     fillPreferencesWindow(window) {
