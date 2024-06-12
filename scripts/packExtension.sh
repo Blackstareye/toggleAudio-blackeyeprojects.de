01c002a94185389036d634db10e22d57470391d6
Blackeye <>
2024-06-12 16:51:49 +0200

    removed not necessary files

--- scripts/packExtension.sh
+++ scripts/packExtension.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 moduleName="toggleAudio@blackeyeprojects.de.zip"
 exportDir="./export/"
-zip -r $exportDir/$moduleName lib/ po/ schemas/ extension.js metadata.json prefs.js README.md stylesheet.css 
+zip -r $exportDir/$moduleName lib/ schemas/ extension.js metadata.json prefs.js README.md
