a15c705bd9559bc1275d6e37c13367a283864b4b
Blackeye <>
2024-09-28 00:16:35 +0200

    fixed language specific options

--- scripts/packExtension.sh
+++ scripts/packExtension.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 moduleName="toggleAudio@blackeyeprojects.de.zip"
 exportDir="./export/"
-zip -r $exportDir/$moduleName lib/ schemas/ extension.js metadata.json prefs.js README.md CHANGELOG.md po/
+#zip -r $exportDir/$moduleName lib/ schemas/ extension.js metadata.json prefs.js README.md CHANGELOG.md po/
+gnome-extensions pack --podir=po --extra-source=lib/ -o $exportDir --force 
